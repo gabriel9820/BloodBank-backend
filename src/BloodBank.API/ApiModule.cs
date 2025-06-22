6fc3c882de8217f2cbfbfb88fa98777065b14868
Gabriel Alves <gabriel_alves_44@live.com>
2025-06-22 20:41:14 -0300

    feat: add swagger authorization

--- src/BloodBank.API/ApiModule.cs
+++ src/BloodBank.API/ApiModule.cs
@@ -22,6 +22,31 @@ public static class ApiModule
                 Version = "v1",
                 Description = "API para gerenciamento de bancos de sangue",
             });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Insira o token JWT no formato: Bearer {token}"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
         });
 
         return services;
