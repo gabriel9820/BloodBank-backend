76662506c0d2437c5a03f1c2bd2839ed0676ae27
Gabriel Alves <gabriel_alves_44@live.com>
2025-07-09 19:02:05 -0300

    test: add unit tests for AddHospitalHandler

---
+++ tests/BloodBank.UnitTests/Application/Commands/AddHospital/AddHospitalHandlerTests.cs
@@ -0,0 +1,64 @@
+using BloodBank.Application.Commands.AddHospital;
+using BloodBank.Application.DTOs.InputModels;
+using BloodBank.Core.Entities;
+using BloodBank.Core.Repositories;
+using BloodBank.Core.ValueObjects;
+
+namespace BloodBank.UnitTests.Application.Commands.AddHospital;
+
+public class AddHospitalHandlerTests
+{
+    private readonly Mock<IHospitalRepository> _hospitalRepositoryMock;
+    private readonly Mock<IUnitOfWork> _uowMock;
+    private readonly AddHospitalHandler _addHospitalHandler;
+
+    public AddHospitalHandlerTests()
+    {
+        _hospitalRepositoryMock = new Mock<IHospitalRepository>();
+        _uowMock = new Mock<IUnitOfWork>();
+        _addHospitalHandler = new AddHospitalHandler(_hospitalRepositoryMock.Object, _uowMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldAddHospital_WhenCommandIsValid()
+    {
+        // Arrange
+        var command = CreateValidCommand();
+
+        _hospitalRepositoryMock.Setup(hr => hr.AddAsync(It.IsAny<Hospital>())).Returns(Task.CompletedTask);
+        _uowMock.Setup(uow => uow.SaveChangesAsync()).ReturnsAsync(1);
+
+        // Act
+        var result = await _addHospitalHandler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+
+        _hospitalRepositoryMock.Verify(hr => hr.AddAsync(It.Is<Hospital>(h =>
+            h.Name == command.Name &&
+            h.LandlineNumber.Equals(new LandlineNumber(command.LandlineNumber)) &&
+            h.Address.Equals(command.Address.ToValueObject())
+        )), Times.Once);
+        _uowMock.Verify(uow => uow.SaveChangesAsync(), Times.Once);
+    }
+
+    private static AddHospitalCommand CreateValidCommand()
+    {
+        var address = new AddressInputModel
+        {
+            Street = "Test Street",
+            Number = "123",
+            Neighborhood = "Test Neighborhood",
+            City = "Test City",
+            State = "TS",
+            ZipCode = "12345-768"
+        };
+
+        return new AddHospitalCommand
+        {
+            Name = "Test Hospital",
+            LandlineNumber = "(11) 1234-5678",
+            Address = address
+        };
+    }
+}
