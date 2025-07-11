a8bd9b8361557c94def79feea242692e067d4d6a
Gabriel Alves <gabriel_alves_44@live.com>
2025-07-10 23:53:27 -0300

    refactor: simplify DeleteDonorCommand constructor syntax

--- src/BloodBank.Application/Commands/DeleteDonor/DeleteDonorCommand.cs
+++ src/BloodBank.Application/Commands/DeleteDonor/DeleteDonorCommand.cs
@@ -3,8 +3,7 @@ using MediatR;
 
 namespace BloodBank.Application.Commands.DeleteDonor;
 
-public class DeleteDonorCommand(
-    int id) : IRequest<Result>
+public class DeleteDonorCommand(int id) : IRequest<Result>
 {
     public int Id { get; private set; } = id;
 }
